a3282f3f3ff1ceadd8a72c39a326238b74762ca4
Dylan Awalt-Conley <web@dylan.ac>
2022-10-20 16:17:04 -0400

    feat: add a require function to work with the legacy js api

--- legacy.js
+++ legacy.js
@@ -1,4 +1,4 @@
-const { isQuoted, quoteString, unquoteString, parseStringLegacy, legacyToString } = require('./utils.js');
+const { isQuoted, quoteString, unquoteString, parseStringLegacy, legacyToString, getAttr } = require('./utils.js');
 const sass = require('sass');
 
 /**
@@ -132,7 +132,54 @@ const fromSass = (object, options={}) => {
     }
 };
 
+/**
+ * An object defining legacy Sass utility functions.
+ */
+const sassFunctions = {
+    /**
+     * Legacy Sass function for importing data from Javascript or JSON files.
+     */
+    'require($module, $properties: (), $parseUnquotedStrings: false, $resolveFunctions: false, $quotes: false)': ($module, $properties, $parseUnquotedStrings, $resolveFunctions, $quotes) => {
+        const moduleName = unquoteString($module.getValue());
+        let properties = fromSass($properties);
+        if (properties && !Array.isArray(properties))
+            properties = [ properties ];
+        const parseUnquotedStrings = $parseUnquotedStrings.getValue();
+        const resolveFunctions = $resolveFunctions.getValue();
+        const quotes = $quotes.getValue()
+        const options = {
+            parseUnquotedStrings,
+            resolveFunctions,
+            quotes
+        };
+        const convert = data => toSass(
+            properties ? getAttr(data, properties) : data,
+            options
+        );
+
+        let mod, paths = [ moduleName, `${process.cwd()}/${moduleName}` ];
+        for (let path of paths) {
+            try {
+                mod = require(path);
+                break;
+            } catch(e) {
+                if (e.code !== 'MODULE_NOT_FOUND') 
+                    throw e;
+                continue;
+            }
+        }
+        if (!mod) throw new Error(`Couldn't find module: ${moduleName}`);
+
+        if (resolveFunctions && typeof mod === 'function')
+            mod = mod();
+        if (mod instanceof Promise)
+            return mod.then(convert);
+        return convert(mod);
+    },
+};
+
 module.exports = {
     toSass,
-    fromSass
+    fromSass,
+    sassFunctions
 };
