e8f2277d7bbb45a42cf49cf36038f103707373b4
Dylan Awalt-Conley <web@dylan.ac>
2023-02-25 15:33:21 -0500

    partial: convert project files to typescript

---
+++ index.ts
@@ -0,0 +1,286 @@
+import sass, {
+  Value,
+  LegacyValue,
+  SassMap,
+  SassList,
+  SassColor,
+  SassNumber,
+  SassString,
+  SassBoolean,
+} from 'sass'
+import { List, OrderedMap } from 'immutable'
+// import * as sass from 'sass'
+import { isQuoted, unquoteString, parseString, getAttr } from './utils'
+
+/**
+ * Converts any Javascript object to an equivalent Sass value.
+ *
+ * This method is recursive and will convert the values of any array or object,
+ * as well as the array or object itself.
+ *
+ * @example
+ * const { toSass } = require('sass-cast');
+ *
+ * const string = toSass('a simple string');
+ * // quoted SassString => '"a simple string"'
+ *
+ * const map = toSass({
+ *   key: 'value',
+ *   nested: {
+ *     'complex//:key': [ null, 4 ],
+ *   }
+ * });
+ * // SassMap => '("key": "value", "nested": ("complex//:key": (null, 4)))'
+ *
+ * @param {*} value - the value to be converted
+ * @param {Object} options
+ * @param {boolean} [options.parseUnquotedStrings=false] - whether to parse unquoted strings for colors or numbers with units
+ * @param {boolean|*[]} [options.resolveFunctions=false] - if true, resolve functions and attempt to cast their return values. if an array, pass as arguments when resolving
+ * @return {Value} - a {@link https://sass-lang.com/documentation/js-api/classes/Value Sass value}
+ */
+
+interface ToSassOptions {
+  parseUnquotedStrings?: boolean
+  resolveFunctions?: boolean | any[]
+}
+
+// type ConvertedToSass<Input> = Input extends null | undefined
+//   ? typeof sassNull
+//   : Input extends boolean
+//   ? SassBoolean
+//   : Input extends number
+//   ? SassNumber
+//   : Input extends string
+//   ? SassString | SassColor | SassNumber
+//   : Input extends any[]
+//   ? SassList
+//   : Input extends object
+//   ? SassMap
+//   : never
+//
+// const toSass = <T extends any>(
+//   value: T,
+//   options: {
+//     parseUnquotedStrings?: boolean
+//     resolveFunctions?: boolean
+//   } = {}
+// ): ConvertedToSass<T> => {
+// const toSass = (value: null | undefined, options: ToSassOptions = {}): typeof sassNull
+
+// type SassValue =
+//   | Value
+//   | typeof sassNull
+//   | SassBoolean
+//   | SassNumber
+//   | SassString
+//   | SassColor
+//   | SassList
+//   | SassMap
+//
+// let value = 'foo' as unknown as Value
+// let test: SassValue = value
+
+// type ToSass = {
+//   (v: null | undefined, o: ToSassOptions): typeof sassNull
+//   (value: boolean, options: ToSassOptions): SassBoolean
+//   // (v: number): SassNumber
+//   (value: any, options: ToSassOptions): SassValue
+// }
+
+// function toSass(v: null | undefined, o?: ToSassOptions): typeof sassNull
+function toSass(v: boolean, o?: ToSassOptions): SassBoolean
+function toSass(v: number, o?: ToSassOptions): SassNumber
+function toSass(
+  v: string,
+  o?: ToSassOptions & { parseUnquotedStrings: true }
+): SassString | SassNumber | SassColor
+function toSass(v: string, o?: ToSassOptions): SassString
+function toSass(v: any[], o?: ToSassOptions): SassList
+// function toSass<F extends (...args: any) => any>(
+//   v: F,
+//   o?: ToSassOptions & { resolveFunctions: true | any[] }
+// ): typeof toSass(v: ReturnType<F>, o?: ToSassOptions)
+function toSass(v: object, o?: ToSassOptions): SassMap
+function toSass(value: any, options: ToSassOptions = {}): Value {
+  let { parseUnquotedStrings = false, resolveFunctions = false } = options
+  if (value instanceof Value) {
+    return value
+  } else if (value === null || value === undefined) {
+    return sass.sassNull
+  } else if (typeof value === 'boolean') {
+    return value ? sass.sassTrue : sass.sassFalse
+  } else if (typeof value === 'number') {
+    return new SassNumber(value)
+  } else if (typeof value === 'string') {
+    if (parseUnquotedStrings && !isQuoted(value)) {
+      let parsed = parseString(value)
+      if (parsed instanceof SassColor || parsed instanceof SassNumber)
+        return parsed
+    }
+    return new SassString(value)
+  } else if (typeof value === 'object') {
+    if (Array.isArray(value)) {
+      let sassList = value.map(value => toSass(value, options))
+      return new SassList(sassList)
+    } else {
+      let sassMap = OrderedMap<any>(value).mapEntries(([key, value]) => [
+        new SassString(key),
+        toSass(value, options),
+      ])
+      return new SassMap(sassMap)
+    }
+  } else if (resolveFunctions && typeof value === 'function') {
+    const args = Array.isArray(resolveFunctions) ? resolveFunctions : []
+    return toSass(value(...args), options)
+  }
+  return sass.sassNull
+}
+
+let foo = toSass(() => 490, { resolveFunctions: true })
+// let v = foo instanceof Value
+console.log(foo)
+
+const colorProperties = [
+  'red',
+  'green',
+  'blue',
+  'hue',
+  'lightness',
+  'saturation',
+  'whiteness',
+  'blackness',
+  'alpha',
+]
+
+/**
+ * Converts Sass values to their Javascript equivalents.
+ *
+ * @example
+ * const { fromSass, toSass } = require('sass-cast');
+ *
+ * const sassString = toSass('a sass string object');
+ * const string = fromSass(sassString);
+ * // 'a sass string object'
+ *
+ * @param {Value} object - a {@link https://sass-lang.com/documentation/js-api/classes/Value Sass value}
+ * @param {Object} options
+ * @param {boolean} [options.preserveUnits=false] - By default, only the values of numbers are returned, not their units. If true, `fromSass` will return numbers as a two-item Array, i.e. [ value, unit ]
+ * @param {boolean} [options.rgbColors=false] - By default, colors are returned as strings. If true, `fromSass` will return colors as an object with `r`, `g`, `b`, and `a`, properties.
+ * @param {boolean} [options.preserveQuotes=false] - By default, quoted Sass strings return their inner text as a string. If true, `fromSass` will preserve the quotes in the returned string value.
+ * @return {*} - a Javascript value corresponding to the Sass input
+ */
+
+const fromSass = (object, options = {}) => {
+  let {
+    preserveUnits = false,
+    rgbColors = false,
+    preserveQuotes = false,
+  } = options
+  if (object instanceof sass.SassBoolean) {
+    return object.value
+  } else if (object instanceof sass.SassNumber) {
+    if (preserveUnits) {
+      return [
+        object.value,
+        object.numeratorUnits.toArray(),
+        object.denominatorUnits.toArray(),
+      ]
+    } else if (object.numeratorUnits.size || object.denominatorUnits.size) {
+      return object.toString()
+    }
+    return object.value
+  } else if (object instanceof sass.SassColor) {
+    if (rgbColors) {
+      return colorProperties.reduce((colorObj, p) => {
+        colorObj[p] = object[p]
+        return colorObj
+      }, {})
+    }
+    return object.toString()
+  } else if (object instanceof sass.SassString) {
+    return preserveQuotes ? object.text : unquoteString(object.text)
+  } else if (object instanceof sass.SassList || List.isList(object)) {
+    let list = []
+    for (
+      let i = 0, value = object.get(i);
+      value !== undefined;
+      i++, value = object.get(i)
+    ) {
+      list.push(fromSass(value, options))
+    }
+    return list
+  } else if (object instanceof sass.SassMap) {
+    return object.contents
+      .mapEntries(([k, v]) => [k.text, fromSass(v, options)])
+      .toObject()
+  } else {
+    return object.realNull
+  }
+}
+
+/**
+ * An object defining Sass utility functions.
+ *
+ * @example <caption>Pass to sass using the JS API</caption>
+ * const { sassFunctions } = require('sass-cast');
+ * const sass = require('sass');
+ *
+ * sass.compile('main.scss', { functions: sassFunctions });
+ */
+const sassFunctions = {
+  /**
+   * Sass function for importing data from Javascript or JSON files.
+   * Calls the CommonJS `require` function under the hood.
+   *
+   * #### Examples
+   *
+   * ```scss
+   * // import config info from tailwindcss
+   * $tw: require('./tailwind.config.js', $parseUnquotedStrings: true);
+   * $tw-colors: map.get($tw, theme, extend, colors);
+   * ```
+   * @name require
+   * @memberof sassFunctions
+   * @param {SassString} $module - Path to the file or module. Relative paths are relative to the Node process running Sass compilation.
+   * @param {SassList} [$properties=()] - List of properties, if you only want to parse part of the module data.
+   * @param {SassBoolean} [$parseUnquotedStrings=false] - Passed as an option to {@link #tosass toSass}.
+   * @param {SassBoolean} [$resolveFunctions=false] - Passed as an option to {@link #tosass toSass}.
+   * @return {Value} - a {@link https://sass-lang.com/documentation/js-api/classes/Value Sass value}
+   */
+  'require($module, $properties: (), $parseUnquotedStrings: false, $resolveFunctions: false)':
+    args => {
+      const moduleName = args[0].assertString('module').text
+      const properties = args[1].realNull && fromSass(args[1].asList)
+      const parseUnquotedStrings = args[2].isTruthy
+      const resolveFunctions = args[3].isTruthy
+      const options = {
+        parseUnquotedStrings,
+        resolveFunctions,
+      }
+      const convert = data =>
+        toSass(properties ? getAttr(data, properties) : data, options)
+
+      let mod,
+        paths = [moduleName, `${process.cwd()}/${moduleName}`]
+      for (let path of paths) {
+        try {
+          mod = require(path)
+          break
+        } catch (e) {
+          if (e.code !== 'MODULE_NOT_FOUND') throw e
+          continue
+        }
+      }
+      if (!mod) throw new Error(`Couldn't find module: ${moduleName}`)
+
+      if (resolveFunctions && typeof mod === 'function') mod = mod()
+      if (mod instanceof Promise) return mod.then(convert)
+      return convert(mod)
+    },
+}
+
+module.exports = {
+  toSass,
+  fromSass,
+  sassFunctions,
+}
---
+++ utils.ts
@@ -0,0 +1,120 @@
+import type { Value, LegacyValue } from 'sass'
+import * as sass from 'sass'
+
+/**
+ * Check if string is quoted
+ * @private
+ * @param {string} str
+ * @return {boolean}
+ */
+const isQuoted = (str: string): boolean => /^['"].*['"]$/.test(str)
+
+/**
+ * Surrounds a string with quotes
+ * @private
+ * @param {string} str
+ * @param {string} q - quotes, double or single
+ * @return {string}
+ */
+const quoteString = (str: string, q: string): string => {
+  if (!q) return str
+  if (isQuoted(str)) {
+    q = str[0]
+    str = str.slice(1, -1)
+  }
+  let r = new RegExp(q, 'g')
+  return q + str.replace(r, '\\' + q) + q
+}
+
+/**
+ * Unquotes a string
+ * @private
+ * @param {string} str
+ * @return {string}
+ */
+const unquoteString = (str: string): string =>
+  isQuoted(str) ? str.slice(1, -1) : str
+
+/**
+ * Parse a string as a Sass object
+ * cribbed from davidkpiano/sassport
+ *
+ * @private
+ * @param {string} str
+ * @return {Value}
+ */
+const parseString = (str: string): Value | string => {
+  let result: Value
+
+  try {
+    sass.compileString(`$_: ___(${str});`, {
+      functions: {
+        '___($value)': args => {
+          result = args[0]
+          return result
+        },
+      },
+    })
+  } catch (e) {
+    return str
+  }
+
+  return result!
+}
+
+/**
+ * Parse a string as a legacy Sass object
+ * cribbed from davidkpiano/sassport
+ *
+ * @private
+ * @param {string} str
+ * @return {LegacyObject}
+ */
+const parseStringLegacy = (str: string): LegacyValue => {
+  let result: LegacyValue
+
+  try {
+    sass.renderSync({
+      data: `$_: ___((${str}));`,
+      functions: {
+        '___($value)': value => {
+          result = value
+          return value
+        },
+      },
+    })
+  } catch (e) {
+    return str
+  }
+
+  return result!
+}
+
+/**
+ * Function to handle 'toString()' methods with legacy API.
+ *
+ * @private
+ * @param {LegacyObject} obj
+ * @return {string}
+ */
+const legacyToString = (obj: LegacyValue): string =>
+  'dartValue' in obj ? (obj.dartValue as Value).toString() : obj.toString()
+
+/**
+ * Return a value from an object and a list of keys.
+ * @private
+ * @param {Object|Array} obj
+ * @param {*[]} attrs
+ */
+const getAttr = (obj: object | any[], attrs: string[]) =>
+  attrs.reduce((o, attr) => o[attr], obj)
+
+export {
+  isQuoted,
+  quoteString,
+  unquoteString,
+  parseString,
+  parseStringLegacy,
+  legacyToString,
+  getAttr,
+}
